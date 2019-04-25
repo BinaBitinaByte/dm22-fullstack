7f74796329530d118d6d117a48563c9f980bafce
Jonathan May <jmay4826@gmail.com>
2019-04-25 12:05:21 -0500

    Add signup route to backend

---
+++ db/signup.sql
@@ -0,0 +1,2 @@
+INSERT INTO bank_user (username, password) VALUES ($1, $2)
+RETURNING * ;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,35 @@
 require("dotenv").config();
 const express = require("express");
-// massive
-// dotenv
-// express-session
-
+const massive = require("massive");
+const authController = require("./controllers/authController");
+const session = require("express-session");
 const app = express();
 const PORT = 5050;
+
+const { CONNECTION_STRING, SESSION_SECRET } = process.env;
+
+massive(CONNECTION_STRING)
+  .then(db => {
+    app.set("db", db);
+    console.log("Database connected");
+  })
+  .catch(err => {
+    console.log("There was a massive error");
+    console.log(err);
+  });
+
+app.use(express.json());
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24
+    }
+  })
+);
+
+app.post("/auth/signup", authController.signup);
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
